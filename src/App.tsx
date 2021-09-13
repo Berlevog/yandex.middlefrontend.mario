c3cfaaa055d10d1b8b94059c68602961c9e283cc
Leonid Artemev <alkamenos@gmail.com>
2021-09-14 01:01:06 +0500

    test gpg
    Signed-off-by: Leonid Artemev <alkamenos@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@
  * Copyright (c) 2021. Written by Leonid Artemev (me@artemev.it)
  */
 import React from "react";
+
 // import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-
+			sdfsd
 		</div>
 	);
 }
