f7c2ecd7c66ec5e4849bb07f788a707fbfae7503
Leonid Artemev <alkamenos@gmail.com>
2021-09-14 11:45:21 +0500

    добавил router
    Signed-off-by: Leonid Artemev <alkamenos@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 /*
  * Copyright (c) 2021. Written by Leonid Artemev (me@artemev.it)
  */
+import { createBrowserHistory } from "history";
 import React from "react";
+import { Route, Router, Switch } from "react-router-dom";
+import { Login, Registration } from "./pages";
 
-// import "./App.css";
+const history = createBrowserHistory();
+const StubComponent = () => (<div>Under construction! 👻</div>);
 
 function App() {
 	return (
 		<div className="App">
-
+			<Router history={history}>
+				<Switch>
+					<Route exact path="/" component={StubComponent} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/registration" component={Registration} />
+					<Route exact path="/app" component={StubComponent} />
+					<Route exact path="/leaderboard" component={StubComponent} />
+					<Route exact path="/forum" component={StubComponent} />
+				</Switch>
+			</Router>
 		</div>
 	);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 /*
  * Copyright (c) 2021. Written by Leonid Artemev (me@artemev.it)
  */
-
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<div>Hello World</div>
+		<App />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
