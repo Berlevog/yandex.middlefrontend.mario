52d7d20825cbbe3051f73a8525d81f8d4e2fd677
Leonid Artemev <alkamenos@gmail.com>
2021-09-14 10:44:50 +0500

    добавил postcss, autoprefixer
    Signed-off-by: Leonid Artemev <alkamenos@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,81 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-module.exports = {
-	mode: 'none',
-	entry: {
-		main: path.join(__dirname, 'src', 'index.tsx')
-	},
-	target: 'web',
-	resolve: {
-		extensions: ['.ts', '.tsx', '.js']
-	},
-	output: {
-		path: path.join(__dirname, '/build'),
-		filename: '[name].[contenthash:5].js'
-	},
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-	module: {
-		rules: [
-			{
-				test: /\.(ts|js)x?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: [
-							'@babel/preset-env',
-							'@babel/preset-react',
-							'@babel/preset-typescript',
-						],
-					},
+module.exports = (env, argv) => {
+	const cssLoader = argv.mode === "production" ? MiniCssExtractPlugin.loader : "style-loader";
+	return {
+		mode: argv.mode,
+		entry: {
+			main: path.join(__dirname, "src", "index.tsx")
+		},
+		target: "web",
+		resolve: {
+			extensions: [".ts", ".tsx", ".js"]
+		},
+		output: {
+			clean: true,
+			path: path.join(__dirname, "/build"),
+			filename: "[name].[contenthash:5].js"
+		},
+
+		module: {
+			rules: [
+				{
+					test: /\.(ts|js)x?$/,
+					exclude: /node_modules/,
+					use: {
+						loader: "babel-loader",
+						options: {
+							presets: [
+								"@babel/preset-env",
+								"@babel/preset-react",
+								"@babel/preset-typescript"
+							]
+						}
 				},
 			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			}
+				{
+					test: /\.css$/i,
+					use: [
+						cssLoader,
+						{
+							loader: "css-loader",
+							options: {
+								importLoaders: 1,
+								modules: true
+							}
+						},
+						{
+							loader: "postcss-loader",
+							options: {
+								postcssOptions: {
+									plugins: [
+										[
+											"autoprefixer",
+											{
+												// Options
+											}
+										]
+									]
+								}
+							}
+						}
+					]
+				}
+			]
+		},
+		devServer: {
+			port: 3000,
+			open: "chrome"
+		},
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: "./public/index.html"
+			}),
+			new MiniCssExtractPlugin({
+				filename: "[name].[contenthash:5].css"
+			})
 		]
-	},
-	devServer: {
-		port: 3000,
-		open: 'chrome'
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index.html'
-		})
-	]
+	}
 };
