d9d618155c80ac3379d6173026d6e26249fcc485
Leonid Artemev <alkamenos@gmail.com>
2021-09-24 16:36:24 +0500

    добавил статику в production сборку
    Signed-off-by: Leonid Artemev <alkamenos@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, argv) => {
-  const cssLoader =
-    argv.mode === "production" ? MiniCssExtractPlugin.loader : "style-loader";
+  const cssLoader = argv.mode === "production" ? MiniCssExtractPlugin.loader : "style-loader";
   return {
     mode: argv.mode,
     entry: {
@@ -23,16 +23,20 @@ module.exports = (env, argv) => {
     module: {
       rules: [
         {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+        },
+        {
           test: /\.(ts|js)x?$/,
           exclude: /node_modules/,
           use: {
             loader: "babel-loader",
             options: {
-              presets: [
-                "@babel/preset-env",
-                "@babel/preset-react",
-                "@babel/preset-typescript",
-              ],
+              presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"],
             },
           },
         },
@@ -73,10 +77,21 @@ module.exports = (env, argv) => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: "./public/index.html",
+        template: "./public/index.tml",
       }),
       new MiniCssExtractPlugin({
-        filename: "[name].[contenthash:5].css",
+        filename: "[name].[contenthash:5]css",
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: "public",
+            to: "",
+            globOptions: {
+              ignore: ["**/index.hml"],
+            },
+          },
+        ],
       }),
     ],
   };
