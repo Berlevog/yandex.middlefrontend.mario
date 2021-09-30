e52eaf37197757ea5b44f0df592e8874aa4a750b
Leonid Artemev <alkamenos@gmail.com>
2021-09-30 17:10:23 +0500

    физика для player
    Signed-off-by: Leonid Artemev <alkamenos@gmail.com>

--- src/engine/Engine.ts
+++ src/engine/Engine.ts
@@ -4,7 +4,14 @@ export namespace Engine {
     y: number;
   }
 
-  export interface IPoint extends IPointData {}
+  export interface IPoint extends IPointData {
+    add: (point: IPoint) => Point;
+    subtract: (point: IPoint) => Point;
+    length: () => number;
+    multiply: (k: number) => Point;
+    divide: (k: number) => Point;
+    addScaled: (point: IPoint, k: number) => Point;
+  }
 
   export interface ISizeData {
     width: number;
@@ -25,15 +32,29 @@ export namespace Engine {
   export type HitSide = "top" | "bottom" | "left" | "right" | undefined;
 
   export interface IHit {
-    side: string;
+    side: string; //todo HitSide
     top: boolean;
     bottom: boolean;
     right: boolean;
     left: boolean;
   }
+
+  export type PhysicalObjectType = "solid" | "resource" | "enemy";
+  export type DisplayObjectProps = {};
+  export type SpriteProps = { texture: ResourceImage } & DisplayObjectProps;
+  export type PhysicalObjectProps = {} & SpriteProps;
+
+  export interface IPhysicalObject {
+    mass: number;
+    type: PhysicalObjectType;
+  }
 }
 
 import EventEmitter from "eventemitter3";
+import { ResourceImage } from "../pages/Game/Resources";
+
+const FREE_FALL_VELOCITY = 9.81;
+const AIR_DENSITY = 1.2;
 
 export function linearEquation(pointA: Engine.IPoint, pointB: Engine.IPoint, x: number) {
   const k = (pointA.y - pointB.y) / (pointA.x - pointB.x);
@@ -45,7 +66,7 @@ export function distanceTo(point: Engine.IPoint, to: Engine.IPoint) {
   return Math.sqrt(Math.pow(to.x - point.x, 2) + Math.pow(to.y - point.y, 2));
 }
 
-export function isAABBColision(rect1: Engine.IRect, rect2: Engine.IRect): boolean {
+export function isAABBCollision(rect1: Engine.IRect, rect2: Engine.IRect): boolean {
   return (
     rect1.x < rect2.x + rect2.width &&
     rect1.x + rect1.width > rect2.x &&
@@ -54,8 +75,8 @@ export function isAABBColision(rect1: Engine.IRect, rect2: Engine.IRect): boolea
   );
 }
 
-export function getAABBColision(rect1: Engine.IRect, rect2: Engine.IRect): Engine.IHit | null {
-  if (isAABBColision(rect1, rect2)) {
+export function getAABBCollision(rect1: Engine.IRect, rect2: Engine.IRect): Engine.IHit | null {
+  if (isAABBCollision(rect1, rect2)) {
     const right = rect2.x + rect2.width;
     const left = rect2.x;
     const bottom = rect2.y + rect2.height;
@@ -86,38 +107,93 @@ export function getAABBColision(rect1: Engine.IRect, rect2: Engine.IRect): Engin
   return null;
 }
 
+export class Point implements Engine.IPoint {
+  public x: number = 0;
+  public y: number = 0;
+
+  constructor({ x, y }: Engine.IPointData) {
+    this.x = x;
+    this.y = y;
+  }
+
+  add(point: Engine.IPoint): Point {
+    this.x += point.x;
+    this.y += point.y;
+    return this;
+  }
+
+  subtract(point: Engine.IPoint): Point {
+    this.x -= point.x;
+    this.y -= point.y;
+    return this;
+  }
+
+  length(): number {
+    return 0;
+  }
+
+  multiply(k: number): Point {
+    this.x /= k;
+    this.y /= k;
+    return this;
+  }
+
+  divide(k: number): Point {
+    this.x /= k;
+    this.y /= k;
+    return this;
+  }
+
+  addScaled(point: Engine.IPoint, k: number) {
+    this.x += k * point.x;
+    this.y += k * point.y;
+    return this;
+  }
+}
+
 export class Rect implements Engine.IRect {
   public width: number;
   public height: number;
   public x: number;
   public y: number;
 
-  constructor(x: Engine.IRect | number, y: number, w: number, h: number) {
-    if (typeof x === "number") {
-      this.width = w;
-      this.height = h;
-      this.x = x;
-      this.y = y;
-    } else {
-      this.width = x.width;
-      this.height = x.height;
-      this.x = x.x;
-      this.y = x.y;
-    }
+  constructor(props: Engine.IRect) {
+    this.width = props.width;
+    this.height = props.height;
+    this.x = props.x;
+    this.y = props.y;
   }
 }
 
 export class DisplayObject extends EventEmitter {
-  public rect: Engine.IRect = { x: 0, y: 0, width: 0, height: 0 };
-  public pivot: Engine.IPoint = { x: 0, y: 0 };
-  private _position: Engine.IPoint = { x: 0, y: 0 };
+  public pivot: Engine.IPoint = new Point({ x: 0, y: 0 });
+  public props: Engine.DisplayObjectProps;
 
-  get position(): Engine.IPoint {
-    return this._position;
+  constructor(props: Engine.DisplayObjectProps = {}) {
+    super();
+    this.props = props;
+  }
+
+  protected _rect: Engine.IRect = { x: 0, y: 0, width: 0, height: 0 };
+
+  get rect(): Engine.IRect {
+    return this._rect;
+  }
+
+  set rect(rect: Engine.IRect) {
+    this._rect = rect;
+    this.position.x = this._rect.x;
+    this.position.y = this._rect.y;
   }
 
   // public scale: number;
   // public rotation: number;
+
+  private _position: Engine.IPoint = new Point({ x: 0, y: 0 });
+
+  get position(): Engine.IPoint {
+    return this._position;
+  }
 
   set position(position: Engine.IPoint) {
     this._position = position;
@@ -137,21 +213,21 @@ export class DisplayObject extends EventEmitter {
     this.rect.height = size.height;
   }
 
-  get positionX() {
+  get x() {
     return this._position.x;
   }
 
-  set positionX(x: number) {
+  set x(x: number) {
     this._position = { ...this._position, x };
     this.rect.x = this._position.x;
     this.rect.y = this._position.y;
   }
 
-  get positionY() {
+  get y() {
     return this._position.y;
   }
 
-  set positionY(y: number) {
+  set y(y: number) {
     this._position = { ...this._position, y };
     this.rect.x = this._position.x;
     this.rect.y = this._position.y;
@@ -175,7 +251,7 @@ export class DisplayObject extends EventEmitter {
 
   // AABB Algorithm
   testHit(rect: Engine.IRect): boolean {
-    return isAABBColision(this.rect, rect);
+    return isAABBCollision(this.rect, rect);
   }
 
   distanceTo(point: Engine.IPoint) {
@@ -183,7 +259,7 @@ export class DisplayObject extends EventEmitter {
   }
 
   getHit(rect: Engine.IRect): Engine.IHit | null {
-    return getAABBColision(this.rect, rect);
+    return getAABBCollision(this.rect, rect);
   }
 
   render(renderer: Engine.IRenderer) {}
@@ -191,4 +267,104 @@ export class DisplayObject extends EventEmitter {
   destroy() {}
 }
 
-export class Sprite extends DisplayObject {}
+export class Sprite extends DisplayObject {
+  public texture: ResourceImage;
+
+  constructor(props: Engine.SpriteProps) {
+    super(props);
+    this.texture = props.texture;
+  }
+
+  setTexture(texture: ResourceImage) {
+    this.texture = texture;
+  }
+}
+
+export class MapObject extends Sprite {
+  collide(object: MapObject) {
+    this.emit("collision", object);
+  }
+}
+
+export class PhysicalObject extends MapObject implements Engine.IPhysicalObject {
+  public mass: number = 0;
+  public type: Engine.PhysicalObjectType = "solid";
+  protected timeStep = 0.02;
+  protected velocityY: number;
+  protected bounciness: number;
+  protected ay: number;
+  protected dragCoefficient: number;
+  protected a: number;
+  private params: any;
+  private interval: NodeJS.Timeout;
+
+  constructor(props: Engine.PhysicalObjectProps) {
+    super(props);
+    this.width = 24;
+    this.height = 24;
+    this.mass = 0.2;
+    this.velocityY = 0;
+    this.bounciness = -0.5;
+    this.ay = 0;
+    this.dragCoefficient = 0.47;
+    this.a = (Math.PI * Math.pow(this.width, 2)) / 10000;
+
+    this.rect = new Rect({ x: 50, y: 50, width: this.width, height: this.height });
+
+    //todo clear
+    this.interval = setInterval(this.physics, this.timeStep * 1000);
+    this.on("collision", this.handleCollision);
+  }
+
+  handleCollision = (object: MapObject) => {
+    if (this.y + this.height > object.y && this.velocityY > 0) {
+      this.velocityY *= this.bounciness;
+      this.y = object.y - this.height;
+    }
+  };
+
+  /* Weight force, which only affects the y-direction (because that's the direction gravity points). */
+  getWeightForce = () => {
+    return this.mass * FREE_FALL_VELOCITY;
+  };
+
+  /* Air resistance force; this would affect both x- and y-directions, but we're only looking at the y-axis in this example. */
+  getAirResistance = () => {
+    return -(0.5 * AIR_DENSITY * this.dragCoefficient * this.a * Math.pow(this.velocityY, 2));
+  };
+
+  physics = () => {
+    let fy = this.getWeightForce() + this.getAirResistance();
+
+    /* Varlet integration for the y-direction */
+    let dy = this.velocityY * this.timeStep + (this.ay / 2) * Math.pow(this.timeStep, 2);
+
+    /* The following line is because the math assumes meters but we're assuming 1 cm per pixel, so we need to scale the results */
+    this.y = Math.round(this.y + dy * 100);
+
+    let new_ay = fy / this.mass;
+    let avg_ay = (new_ay + this.ay) / 2;
+    this.velocityY += avg_ay * this.timeStep;
+  };
+
+  render(renderer: Engine.IRenderer) {
+    const { context } = renderer;
+
+    context.save();
+    context.drawImage(this.texture.img, this.x, this.y, this.width, this.height);
+    context.restore();
+  }
+
+  destroy() {
+    this.removeAllListeners("collision");
+    clearInterval(this.interval);
+  }
+}
+
+// type BrickProps = {} & SpriteProps;
+
+// export class Brick extends Sprite {
+//   constructor(props: BrickProps) {
+//     super({ ...props, texture: new ResourceImage('images/cloud.png"') });
+//   }
+// }
--- src/engine/World.ts
+++ src/engine/World.ts
@@ -1,87 +1,106 @@
 import { ResourceImage } from "../pages/Game/Resources";
 import Clouds from "./Clouds";
-import { Engine, getAABBColision, Sprite } from "./Engine";
-import Player from "./Player";
+import { Engine, MapObject, PhysicalObject, Rect, Sprite } from "./Engine";
 
 const SHIFT_WIDTH: number = 120;
 const LAND_Y: number = 207;
 
+type WorldProps = {
+  player: PhysicalObject;
+};
+
 export default class World extends Sprite {
   public sprite: ResourceImage;
-  private player: Player;
+  private player: PhysicalObject;
   private groundRects: Engine.IRect[];
   private debugAreas: boolean = false;
   private clouds: Clouds;
+  private mapObjects: MapObject[];
 
-  constructor(player: Player) {
-    super();
+  constructor(props: WorldProps) {
+    super({ ...props, texture: new ResourceImage("images/world.png") });
     this.clouds = new Clouds();
-    this.player = player;
+    this.player = props.player;
     this.sprite = new ResourceImage("images/world.png");
 
     this.groundRects = [
       { x: 0, y: LAND_Y, width: 1105, height: 32 },
-      { x: 448, y: LAND_Y - 32, width: 32, height: 64 },
+      // { x: 448, y: LAND_Y - 32, width: 32, height: 64 },
       { x: 608, y: LAND_Y - 48, width: 32, height: 80 },
     ];
+
+    const obj1 = new MapObject({ texture: new ResourceImage("images/world.png") });
+    obj1.rect = new Rect({ x: 0, y: LAND_Y, width: 1105, height: 32 });
+    this.mapObjects = [obj1];
   }
 
-  checkCollisions() {
+  detectCollisions() {
+    this.mapObjects.forEach((object) => {
+      if (this.player.testHit(object.rect)) {
+        this.player.collide(object);
+        object.collide(this.player);
+      }
+    });
+  }
+
+  /*checkCollisions() {
     //ground
     let isFalling = true;
 
     this.groundRects.forEach((ground) => {
       if (this.player.testHit(ground)) {
-        const aabbColision = getAABBColision(ground, this.player.rect);
+        const aabbColision = getAABBCollision(ground, this.player.rect);
 
         if (aabbColision?.bottom) {
-          this.player.positionY = ground.y - this.player.rect.height + 1;
+          this.player.y = ground.y - this.player.rect.height + 1;
           isFalling = false;
         } else {
           // this.player.setPlayerState(PlayerState.FALLING);
           if (aabbColision?.top) {
-            this.player.positionY = ground.y + ground.height - 1;
+            this.player.y = ground.y + ground.height - 1;
           }
           if (aabbColision?.left) {
-            this.player.positionX = ground.x - this.player.rect.width - 1;
+            this.player.x = ground.x - this.player.rect.width - 1;
           }
           if (aabbColision?.right) {
-            this.player.positionX = ground.x + ground.width + 1;
+            this.player.x = ground.x + ground.width + 1;
           }
         }
       } else {
       }
-      this.player.isFalling = isFalling;
+      // this.player.isFalling = isFalling;
     });
-  }
+  }*/
 
   shiftWorld(canvas: HTMLCanvasElement) {
-    if (this.player.positionX > canvas.width - SHIFT_WIDTH && this.sprite.img.width > -this.positionX + canvas.width) {
+    if (this.player.x > canvas.width - SHIFT_WIDTH && this.sprite.img.width > -this.x + canvas.width) {
       if (this.sprite.img.width) {
-        const shift = Math.min(this.sprite.img.width + this.positionX - canvas.width, SHIFT_WIDTH);
-        this.positionX = this.positionX - shift;
-        this.player.positionX = this.player.positionX - shift;
+        const shift = Math.min(this.sprite.img.width + this.x - canvas.width, SHIFT_WIDTH);
+        this.x = this.x - shift;
+        this.player.x = this.player.x - shift;
         this.groundRects.forEach((rect) => {
           rect.x = rect.x - shift;
         });
       }
 
       this.clouds.getClouds().forEach((cloud) => {
-        cloud.positionX = cloud.positionX - SHIFT_WIDTH;
+        cloud.x = cloud.x - SHIFT_WIDTH;
       });
     }
-    if (this.player.positionX >= canvas.width) {
-      this.player.positionX = canvas.width - this.player.size.width - 24;
+    if (this.player.x >= canvas.width) {
+      this.player.x = canvas.width - this.player.size.width - 24;
     }
+
+    // this.mapObjects = this.mapObjects.filter((object) => object.positionX + object.width - SHIFT_WIDTH > 0);
   }
 
   render(renderer: Engine.IRenderer) {
     const { canvas, context } = renderer;
-    this.checkCollisions();
+    this.detectCollisions();
     this.shiftWorld(canvas);
 
     context.save();
-    context.drawImage(this.sprite.img, this.positionX, this.positionY);
+    context.drawImage(this.sprite.img, this.x, this.y);
 
     if (this.debugAreas) {
       context.fillStyle = "blue";
