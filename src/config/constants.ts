df9592bd90ead127c4f7ca4506cd2aa3e7b912b2
shoom1337 <kirillpavlik@mail.ru>
2021-10-05 12:42:55 +0300

    Добавил вывод ошибок на странице Registration

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -3,3 +3,4 @@ export const NO_CONTENT = "No content";
 export const THREAD_AVATAR_WIDTH = 150;
 export const DEFAULT_AVATAR = "/images/default-avatar.png";
 export const THREAD_CONTENT_PREVIEW_LENGTH = 100;
+export const UNKNOWN_ERROR = "Unknown error";
