aa8b07e06a45c8a80415c1889739dd844032283c
shoom1337 <kirillpavlik@mail.ru>
2021-10-06 13:50:40 +0300

    Пробую thunk на запросах лидерборда

--- src/pages/Leaderboard/TopPlayersList.tsx
+++ src/pages/Leaderboard/TopPlayersList.tsx
@@ -1,16 +1,47 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Title from "./Title";
-import { data } from "./mockData";
+import { getResults, UserResult } from "../../services/leaderboard";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { loadPending, loadSuccess, loadFailed } from "../../store/reducers/loader";
+import { Loader } from "../../components/Loader";
 
 export default function TopPlayersList() {
+  const [results, setResults] = useState<UserResult[]>([]);
+
+  const dispatch = useAppDispatch();
+
+  function getLeaderboardResults() {
+    return (dispatch) => {
+      dispatch(loadPending());
+      getResults()
+        .then((response) => {
+          dispatch(loadSuccess());
+          setResults(response);
+        })
+        .catch(() => dispatch(loadFailed()));
+    };
+  }
+
+  const updateResults = useCallback(() => {
+    dispatch(getLeaderboardResults());
+  }, []);
+
+  useEffect(updateResults, []);
+
+  const isLoading = useAppSelector(({ loader }) => loader.loading);
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <React.Fragment>
       <Title>Топ игроков</Title>
+
       <Table size="small">
         <TableHead>
           <TableRow>
@@ -21,12 +52,12 @@ export default function TopPlayersList() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((entity) => (
-            <TableRow key={entity.id}>
-              <TableCell>{entity.name}</TableCell>
-              <TableCell>{entity.score}</TableCell>
-              <TableCell>{entity.coins}</TableCell>
-              <TableCell>{entity.time}</TableCell>
+          {results.map(({ data }) => (
+            <TableRow key={data.score}>
+              <TableCell>{data.name}</TableCell>
+              <TableCell>{data.score}</TableCell>
+              <TableCell>{data.coins}</TableCell>
+              <TableCell>{data.time}</TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,8 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 import reducer from "./reducers";
 
 export default function configureStore(preloadedState) {
-  const store = createStore(reducer, preloadedState);
+  const store = createStore(reducer, preloadedState, applyMiddleware(thunk));
   return store;
 }
