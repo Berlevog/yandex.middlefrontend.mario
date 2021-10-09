75cde1c733d35b6277ecf3f1d9355f9066007052
shoom1337 <kirillpavlik@mail.ru>
2021-10-10 00:09:08 +0300

    Косметика

--- src/pages/Profile/Profile.test.tsx
+++ src/pages/Profile/Profile.test.tsx
@@ -2,8 +2,10 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Profile } from ".";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<Profile />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("render profile", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Profile />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
--- src/store/store.ts
+++ src/store/store.ts
@@ -6,9 +6,3 @@ export const store = configureStore({ reducer });
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-// import { createStore, applyMiddleware } from "redux";
-
-// export default function configureStore(preloadedState) {
-//   const store = createStore(reducer, preloadedState, applyMiddleware(thunk));
-//   return store;
-// }
