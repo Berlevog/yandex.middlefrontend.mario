c83399f07f6e8f9a6e0188cc1efbf75d0df7522e
shoom1337 <kirillpavlik@mail.ru>
2021-10-12 14:00:02 +0300

    Фикс теста

--- src/pages/Profile/Profile.test.tsx
+++ src/pages/Profile/Profile.test.tsx
@@ -1,11 +1,23 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { mount, ReactWrapper } from "enzyme";
 import { Profile } from ".";
+import store from "../../store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 describe("render profile", () => {
+  let wrapper: ReactWrapper;
+  beforeEach(() => {
+    wrapper = mount(
+      <BrowserRouter>
+        <Provider store={store}>
+          <Profile />
+        </Provider>
+      </BrowserRouter>
+    );
+  });
   it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<Profile />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    expect(wrapper.length).toEqual(1);
   });
 });
