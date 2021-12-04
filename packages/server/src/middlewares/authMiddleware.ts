506d00bb49b18cb1b8b602227dcfa7e5170f192d
shoom1337 <kirillpavlik@mail.ru>
2021-12-04 19:18:27 +0300

    Победил auth middleware

--- packages/server/src/middlewares/authMiddleware.ts
+++ packages/server/src/middlewares/authMiddleware.ts
@@ -1,36 +1,49 @@
 import axios from "axios";
 import { NextFunction, Request, Response } from "express";
 
-const AUTH_URL = "https://ya-praktikum.tech/api/v2/auth/user";
+const YANDEX_AUTH_URL = "https://ya-praktikum.tech/api/v2/auth/user";
+
+const cookieToString = (cookies: Record<string, string>): string => {
+  let res = "";
+
+  if (cookies) {
+    Object.entries<string>(cookies).forEach(([key, value], ind) => {
+      res += `${ind === 0 ? "" : " "}${key}=${value};`;
+    });
+  }
+
+  return res;
+};
 
 export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
   enum protectedRoutes {
-    "/forum" = "forum",
-    "/login" = "login",
+    "/forum" = 1,
+    "/profile" = 1,
+    "/leaderboard" = 1,
   }
 
+  const cookies = req.cookies;
+
   //@ts-ignore
-  const isProtectedRoute = protectedRoutes[req.url];
+  const isProtectedRoute = !!protectedRoutes[req.url];
 
-  console.log(req.cookies);
-  console.log("Signed Cookies: ", req.signedCookies);
+  if (isProtectedRoute) {
+    try {
+      const { data: user } = await axios.get(YANDEX_AUTH_URL, {
+        headers: { Cookie: cookieToString(cookies) },
+      });
+
+      res.locals.user = user;
+
+      return next();
+    } catch (e) {
+      //@ts-ignore
+      console.log("AUTH Middleware: ", e.message);
+      req.url = "/login";
+      res.redirect("/login");
+      return;
+    }
+  }
+
   next();
-  // if (isProtectedRoute) {
-  //   try {
-  //     // const cookie = `authCookie=${req.cookies.authCookie}`;
-
-  //     console.log(req.cookies);
-
-  //     // const { data } = await axios.get(AUTH_URL, {
-  //     //   headers: { Cookie: cookie },
-  //     // });
-
-  //     // console.log(data);
-  //     next();
-  //   } catch (e) {
-  //     //@ts-ignore
-  //     console.log("redirect to /login", e, e.message);
-  //   }
-  // }
-  // next();
 }
